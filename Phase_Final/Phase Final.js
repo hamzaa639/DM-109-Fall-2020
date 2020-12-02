f9eb73486e926ac5dd180d0ec782842c755c1e39
Ahtisham Ali <68895242+ahtisham0341@users.noreply.github.com>
2020-12-02 21:29:39 +0500

    Update Phase Final.js

--- Phase_Final/Phase Final.js
+++ Phase_Final/Phase Final.js
@@ -16,10 +16,10 @@ window.onload = function () {
 			dataPoints: [
 				{ label: "4 x 4",  y: 0.15 },
 				{ label: "8 x 8", y: 0.25  },
-				{ label: "20 x 20", y: 1  },
-				{ label: "30 x 3x",  y: 5  },
+				{ label: "20 x 20", y: 2  },
+				{ label: "30 x 3x",  y: 4  },
 				{ label: "50 x 50",  y: 4.5  },
-				{ label: "80 x 80",  y: 9  },
+				{ label: "80 x 80",  y: 8  },
 				{ label: "100 x 100",  y: 10  },
 			]
 		}
